9c134e220662e6c3733bca27c1f0d4d5cc7b6c39
Anatolii Fenin <fenintolja@gmail.com>
2024-11-26 09:37:59 +0200

    header routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header';
 import type { Metadata } from 'next';
 import { Urbanist } from 'next/font/google';
 import './globals.css';
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className=" bg-slate-900 text-slate-100">
-      <body className={`${urbanist.className}  antialiased`}>{children}</body>
+      <body className={`${urbanist.className}  antialiased`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
