3bbadcb1da53174ebb26d3de5bc6f8215619ad7b
Anatolii Fenin <fenintolja@gmail.com>
2024-12-02 09:50:12 +0200

    generate metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import type { Metadata } from 'next';
 import { Urbanist } from 'next/font/google';
 import './globals.css';
-import Footer from '@/components/Footer';
+import { PrismicPreview } from '@prismicio/next';
+import { createClient, repositoryName } from '@/prismicio';
 
 const urbanist = Urbanist({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Anatolii Fenin Portfolio',
-  description: 'Created by Tolik',
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const settings = await client.getSingle('settings');
+
+  return {
+    title: settings.data.meta_title,
+    description: settings.data.meta_description,
+  };
+}
 
 export default function RootLayout({
   children,
@@ -27,6 +34,7 @@ export default function RootLayout({
         <div className="absolute inset-0 -z-50 max-h-screen background-gradient"></div>
         <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
       </body>
+      <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
 }
